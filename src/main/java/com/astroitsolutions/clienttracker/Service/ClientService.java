584ab9a4bd1bfb7f483c233aaf5e034723b8988b
Arash Dehdari <arde1369@yahoo.com>
2023-01-29 23:48:36 -0600

    Added junit 5 and mockito deps and removed some unused imports

--- src/main/java/com/astroitsolutions/clienttracker/Service/ClientService.java
+++ src/main/java/com/astroitsolutions/clienttracker/Service/ClientService.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.astroitsolutions.clienttracker.Entity.Client;
 import com.astroitsolutions.clienttracker.Entity.Product;
